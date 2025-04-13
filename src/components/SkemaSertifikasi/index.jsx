be54893775769c80f37cc9199fad4fd6ddca8bee
n0tavaliduser <stevanoputra38@gmail.com>
2025-04-14 01:21:23 +0700

    fix(SkemaSertifikasi): add error handling for image loading in CardItem component

--- src/components/SkemaSertifikasi/index.jsx
+++ src/components/SkemaSertifikasi/index.jsx
@@ -1,9 +1,13 @@
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
+const handleImageError = (e) => {
+  e.target.src = '/src/assets/Skema/sekretaris.png';
+};
+
 const CardItem = ({ id, title, subtitle, imageUrl }) => (
   <Card className="relative group overflow-hidden shadow-lg rounded-lg bg-white transition-all duration-300 ease-in-out hover:scale-105">
     <CardContent className="relative p-0">
-      <img src={imageUrl} alt={title} className="w-full h-64 object-cover transition-all duration-300 group-hover:opacity-60" />
+      <img src={imageUrl} alt={title} className="w-full h-64 object-cover transition-all duration-300 group-hover:opacity-60" onError={handleImageError} />
       <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-orange-500 bg-opacity-50">
         <div className="text-white text-center">
           s<p className="text-sm md:text-lg lg:text-xl font-bold text-[#102640]">{title}</p>
